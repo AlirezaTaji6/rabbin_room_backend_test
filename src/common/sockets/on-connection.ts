d3886408b4d47ea8ca0e393ab17697ff46b24cd4
Alireza Taji <alirezataji6@gmail.com>
2022-05-15 13:53:27 +0430

    feat: add on connection

--- src/common/sockets/on-connection.ts
+++ src/common/sockets/on-connection.ts
@@ -1,8 +1,10 @@
 import { Server, Socket } from 'socket.io'
+import { onJoinRoomSocket } from '../../rooms/on-join-room.socket';
 import { onSwitchFilePageSocket } from '../../switch-file-page/on-switch-file-page.socket';
 
 export function onConnection(io: Server) {
     return async (socket: Socket) => {
         socket.on('switch-file-page', onSwitchFilePageSocket(io, socket))
+        socket.on('join-room', onJoinRoomSocket(io, socket))
     }
 }
